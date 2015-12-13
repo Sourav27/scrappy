836ae883188ec28a542919d9df5164601a7072b1
sourav27 <debnath.sourabh@gmail.com>
2015-12-13 17:26:52 +0530

    using sendgrid mailer

--- config/application.rb
+++ config/application.rb
@@ -16,15 +16,8 @@ module Scrappy
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
-
-config.before_configuration do
-  env_file = File.join(Rails.root, 'config', 'local_env.yml')
-  YAML.load(File.open(env_file)).each do |key, value|
-    ENV[key.to_s] = value
-  end if File.exists?(env_file)
-end
-config.assets.initialize_on_precompile = false
+    
+    config.assets.initialize_on_precompile = false
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
