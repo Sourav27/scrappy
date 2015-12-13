cc1ed7645aae9ee15b9f642c705df4eb3e887eb1
sourav27 <debnath.sourabh@gmail.com>
2015-12-13 16:46:20 +0530

    actionmailer fix#1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   #ActionMailer
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'scrapwala.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
