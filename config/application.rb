c222d83177bc42336838a07970508a19b5ed3303
sourav27 <debnath.sourabh@gmail.com>
2015-12-13 14:23:40 +0530

    devise heroku fix#1

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,16 @@ module Scrappy
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+
+config.before_configuration do
+  env_file = File.join(Rails.root, 'config', 'local_env.yml')
+  YAML.load(File.open(env_file)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(env_file)
+end
+config.assets.initialize_on_precompile = false
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -25,12 +35,3 @@ module Scrappy
   end
 end
 
-# Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
-    
-config.before_configuration do
-  env_file = File.join(Rails.root, 'config', 'local_env.yml')
-  YAML.load(File.open(env_file)).each do |key, value|
-    ENV[key.to_s] = value
-  end if File.exists?(env_file)
-end
