c48ea9e001cd71959b2422dca6152f13ea20bae9
sourav27 <debnath.sourabh@gmail.com>
2015-12-15 12:43:02 +0530

    actionmailer fix#2

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# ActionMailer SMTP settings
+ActionMailer::Base.smtp_settings = {
+	  :address              => "smtp.sendgrid.com",
+      :port                 => "587",
+      :domain               => "scrapwala.herokuapp.com",
+      :user_name            => ENV["SENDGRID_USERNAME"],
+      :password             => ENV["SENDGRID_PASSWORD"],
+      :authentication       => :plain,
+      :enable_starttls_auto => true,
+	  :openssl_verify_mode  => 'none' 
+}
